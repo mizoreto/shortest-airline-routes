557c8ad069ef85b3a2517bc0a6daf358856efc04
Ziyi Wang <ziyiw8@linux-01.ews.illinois.edu>
2021-11-27 19:12:30 -0600

    Read Routes

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include "read_airport.h"
 #include "read_airport.cpp"
+#include "read_routes.h"
+#include "read_routes.cpp"
+#include <vector>
 
 int main() {
     map<string, airport> test = readRawData();
+    map<string, pair<vector<string>, vector<double> > > test_1 = read_routes();
     return 0;
 }
