f8e5e141cebc10570141588eba640d08d87906b5
Yuhua Weng <yuhua2@siebl-0224-34.ews.illinois.edu>
2021-12-07 10:02:56 -0600

    add BFS traversal pseudocode

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -1,1 +1,34 @@
 #include "algorithms.h"
+
+// BFS algorithm
+
+
+// reference: https://www.programiz.com/dsa/graph-bfs
+
+
+// void Graph::BFS(int startVertex) {
+//   visited = new bool[numVertices];
+//   for (int i = 0; i < numVertices; i++)
+//     visited[i] = false;
+
+//   list<int> queue;
+
+//   visited[startVertex] = true;
+//   queue.push_back(startVertex);
+
+//   list<int>::iterator i;
+
+//   while (!queue.empty()) {
+//     int currVertex = queue.front();
+//     cout << "Visited " << currVertex << " ";
+//     queue.pop_front();
+
+//     for (i = adjLists[currVertex].begin(); i != adjLists[currVertex].end(); ++i) {
+//       int adjVertex = *i;
+//       if (!visited[adjVertex]) {
+//         visited[adjVertex] = true;
+//         queue.push_back(adjVertex);
+//       }
+//     }
+//   }
+// }
