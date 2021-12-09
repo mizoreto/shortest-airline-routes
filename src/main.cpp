01508e4bc79b309a7d2b75cd0bc4f5eaf8e4fab6
Ziyi Wang <ziyiw8@linux-02.ews.illinois.edu>
2021-12-08 19:32:03 -0600

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include "read_airport.h"
 #include "read_airport.cpp"
+#include "read_routes.h"
+#include "read_routes.cpp"
+#include "algorithms.h"
+#include "algorithms.cpp"
 
 #include <vector>
 
@@ -8,5 +12,7 @@ int main() {
     apts.readRawData();
     apts.readCleanData();
     // cout << apts.airports["TRF"]->ID << endl;
+    map<string, pair<vector<string>, vector<double> > > test_1 = read_routes();
+    BFS("AER");
     return 0;
 }
