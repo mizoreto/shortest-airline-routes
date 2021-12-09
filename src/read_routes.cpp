ffed37a417bef31d8ddabf91f40573ef85043608
Ziyi Wang <ziyiw8@linux-02.ews.illinois.edu>
2021-12-08 20:15:51 -0600

    added comments

--- src/read_routes.cpp
+++ src/read_routes.cpp
@@ -20,35 +20,46 @@ map<string, pair<vector<string>, vector<double> > > read_routes() {
     ifstream inFile;
     Airports raw_data;
     raw_data.readCleanData();
-    map<string, Airports::airport*> airport_data = raw_data.airports;
+    map<string, Airports::airport*> airport_data = raw_data.airports;//initialize airport data
     
     inFile.open("routes.dat");
     if (!inFile) {
         cout<<"Failed to open file"<<endl;
         exit(1); //terminate program
     }
+    //file opened succesfully
     map<string, pair<vector<string>, vector<double> > > result_routes;
     string line_text;
+    //traverse by line in routes.dat
     while (getline(inFile, line_text)) {
         vector<string> v;
         stringstream ss(line_text);
+        //store every element on the current line into a string vector
         while (ss.good()) {
             string substr;
             getline(ss, substr, ',');
             v.push_back(substr);
         }
-        
         //cout<<"v[2]: "<<v[2]<<"  "<<"v[4]: "<<v[4]<<endl;
+
+        //skip to the next line if the airports in the current
+        //line doesn't exist in airports.dat
         if (airport_data.find(v[2]) == airport_data.end() 
             || airport_data.find(v[4]) == airport_data.end()) {
                 continue;
         }
+        //add the destination airport to the vector that stores all
+        //the destinations of the corresponding source airport
         result_routes[v[2]].first.push_back(v[4]);
+        
+        //get the latitutdes and longitudes of the source and destination
+        //airport and calculate distance in between
         double src_lat = airport_data[v[2]]->lat;
         double src_long = airport_data[v[2]]->lon;
         double dest_lat = airport_data[v[4]]->lat;
         double dest_long = airport_data[v[4]]->lon;
         double src_dest_dist = haversine_dist(src_lat, src_long, dest_lat, dest_long);
+        //store the distance into the corresponding vector
         result_routes[v[2]].second.push_back(src_dest_dist);
     }
     inFile.close();
@@ -75,8 +86,7 @@ double haversine_dist(double lat_1, double long_1, double lat_2, double long_2) 
     double a = pow(sin(lat_diff / 2), 2) + cos(cvrtToRad(lat_1)) * 
            cos(cvrtToRad(lat_2)) * sin(lon_diff / 2) * sin(lon_diff / 2);
     double c = 2 * atan2(sqrt(a), sqrt(1 - a));
-    return E_RADIUS * c;
-    
+    return E_RADIUS * c;   
 }
 
 double cvrtToRad(double degree) {
