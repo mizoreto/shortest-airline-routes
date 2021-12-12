b15a064eca697a15b45e77035bd3b3324245b44d
Boyang Yu <boyangy4@siebl-0224-33.ews.illinois.edu>
2021-12-11 19:17:45 -0600

    Cleaned Up read_airport, and added ability to read different URLs.

--- src/read_airport.cpp
+++ src/read_airport.cpp
@@ -9,14 +9,14 @@
  * Reads raw data from airports.dat, the file from openflights.org.
  * Then addes the data into the class variable airports, which is a map.
 */
-void Airports::readRawData() {
-    ifstream file("airports.dat"); //setting up variables.
+void Airports::readRawData() {readRawData("airports.dat");}
+void Airports::readRawData(string URL) {
+    ifstream file(URL.c_str()); //setting up variables.
     string lineText;
     int count = 0;
-    // cout << "line 13" << endl;
     while (getline(file, lineText)) {//this loop reads all every line of airports.dat
         string parts[14];
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 8; i++) {//for every segment of the line seperated by commas, splits the string and put them in parts[]
             size_t commaIndex = lineText.find(',');
             size_t specialCommaIndex = lineText.find(", ");
             if (specialCommaIndex != string::npos && specialCommaIndex <= commaIndex) 
@@ -25,49 +25,46 @@ void Airports::readRawData() {
             lineText.erase(0, commaIndex + 1);
         }
         airport * newAirport = new airport(); //creating a new airport object and setting up its variables.
-        if (parts[4] != "\\N") newAirport->ID = parts[4];
+        if (parts[4] != "\\N") newAirport->ID = parts[4]; //if airport does not have 3-letter ID, use 4-letter ID instead.
         else newAirport->ID = parts[5];
         newAirport->ID.erase(newAirport->ID.begin());
         newAirport->ID.erase(newAirport->ID.end()-1);
         newAirport->lat = strtod(parts[6].c_str() , NULL);
         newAirport->lon = strtod(parts[7].c_str() , NULL);
-        newAirport->degree = -1;
+        newAirport->degree = -1; //default degree to -1, meaning no degree has been added to this airport, yet.
         airports.insert(pair<string, airport*> (newAirport->ID, newAirport));
-        
+
         count++;
     }
-    // map<string, Airports::airport*>::iterator it = airports.begin();
-    // advance(it, 104);
-    cout << "airports has size " << airports.size() << endl;// cout << "This airport is " << airports["MAG"]->ID << endl;
     file.close();
-    cout << "Task Complete: raw data from airports.dat is added to class variable airports of type map<string, Airports::airport*>" << endl;
+    //cout << "Task Complete: raw data from " << URL << " is added to class variable airports of type map<string, Airports::airport*>" << endl;
     writeCleanData();
 }
-
-void Airports::writeCleanData() {
-    if (airports.empty()) {
-        cout << "Airports is empty" << endl;
+void Airports::writeCleanData() {writeCleanData("airports_clean.dat");}
+void Airports::writeCleanData(string URL) {
+    if (airports.empty()) {//if the map airports is empty, then nothing to write.
+        cout << "Error: Airports is empty" << endl;
         return;
     }
-    ofstream outFile("airports_clean.dat");
-    for (map<string, airport*>::iterator it = airports.begin(); it != airports.end(); ++it) {
+    ofstream outFile(URL.c_str());
+    for (map<string, airport*>::iterator it = airports.begin(); it != airports.end(); ++it)
         outFile << it->second->ID << "," << it->second->lat << "," << it->second->lon << endl;
-    }
     outFile.close();
-    cout << "Task Complete: data from the map airports is written to the file airports_clean.dat" << endl;
+    //cout << "Task Complete: data from the map airports is written to the file " << URL << endl;
 }
 
-void Airports::readCleanData() {
-    ifstream file("airports_clean.dat"); //setting up variables.
+void Airports::readCleanData() {readCleanData("airports_clean.dat");}
+void Airports::readCleanData(string URL) {
+    ifstream file(URL.c_str()); //setting up variables.
     if (file.fail()) {
-        cout << "Task Failed: the file airports_clean.dat is not found" << endl;
+        cout << "Error: the file " << URL << " is not found" << endl;
         return;
     }
     if (!airports.empty()) airports.clear();
     string lineText;
      while (getline(file, lineText)) {//this loop reads all every line of airports_clean.dat
         string parts[3];
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {//for every segment of the line seperated by commas, splits the string and put them in parts[]
             size_t commaIndex = lineText.find(',');
             parts[i] = lineText.substr(0, commaIndex);
             lineText.erase(0, commaIndex + 1);
@@ -78,6 +75,5 @@ void Airports::readCleanData() {
         newAirport->lon = strtod(parts[2].c_str() , NULL);
         airports.insert(pair<string, airport*> (newAirport->ID, newAirport));
     }
-    cout << "airports has size " << airports.size() << endl;
-    cout << "Task Complete: data from airports_clean.dat is added to class variable airports of type map<string, Airports::airport*>" << endl;
+    //cout << "Task Complete: data from " << URL << " is added to class variable airports of type map<string, Airports::airport*>" << endl;
 }
