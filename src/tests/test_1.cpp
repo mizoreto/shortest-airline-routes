cc877fdf541c638562bf74930508ee986ee618f6
Ziyi Wang <ziyiw8@linux-28.ews.illinois.edu>
2021-12-12 17:52:39 -0600

    changed tests

--- src/tests/test_1.cpp
+++ src/tests/test_1.cpp
@@ -1,5 +1,7 @@
 #include "../algorithms.h"
+#include "../algorithms.cpp"
 #include "../read_routes.h"
+#include "../read_routes.cpp"
 #include "../read_airport.h"
 #include "../read_airport.cpp"
 #include "../catch/catch.hpp"
@@ -23,37 +25,26 @@ using namespace std;
 // ,,PEK,,UGL,,,,
 // ,,SAN,,PEK,,,,
 
-// TEST_CASE("Test Dijkstra") {
-//     Airports test_airports;
-//     routes test_routes;
-//     Airports::airport ARC("ARC", 40.1013125, -88.2381926, -1);
-//     Airports::airport UGL("UGL", 40.1046698, -88.2290223, -1);
-//     Airports::airport WAL("WAL", 40.146127, -88.255618, -1);
-//     Airports::airport SAN("SAN", 37.2965314, -122.0976163, -1);
-//     Airports::airport PEK("PEK", 40.0798573, 116.6009234, -1);
-//     //UGL distance array index 0
-//     test_routes.routes_map["UGL"].first.push_back("ARC");
-//     test_routes.routes_map["UGL"].second.push_back(
-//         test_routes.haversine_dist(40.1046698, -88.2290223, 40.1013125, -88.2381926));
-//     //UGL distance array index 1
-//     test_routes.routes_map["UGL"].first.push_back("WAL");
-//     test_routes.routes_map["UGL"].second.push_back(
-//         test_routes.haversine_dist(40.1046698, -88.2290223, 40.146127, -88.255618));
-//     //UGL distance array index 2
-//     test_routes.routes_map["UGL"].first.push_back("PEK");
-//     test_routes.routes_map["UGL"].second.push_back(
-//         test_routes.haversine_dist(40.1046698, -88.2290223, 40.0798573, 116.6009234));
-//     //PEK distance array index 0
-//     test_routes.routes_map["PEK"].first.push_back("UGL");
-//     test_routes.routes_map["PEK"].second.push_back(
-//         test_routes.haversine_dist(40.0798573, 116.6009234, 40.1046698, -88.2290223));
-//     //SAN distance array index 0
-//     test_routes.routes_map["SAN"].first.push_back("PEK");
-//     test_routes.routes_map["SAN"].second.push_back(
-//         test_routes.haversine_dist(37.2965314, -122.0976163, 40.0798573, 116.6009234));
-//     REQUIRE(test_routes.routes_map["UGL"].second[0] < test_routes.routes_map["UGL"].second[2]);
-//     REQUIRE(test_routes.routes_map["UGL"].second[1] < test_routes.routes_map["UGL"].second[2]);
-// }
+// pair<double, vector<string> > dijkstra(string startVertex, 
+//             string endVertex, map<string, pair<vector<string>, vector<double> > > routes_data);
+
+
+TEST_CASE("Test Dijkstra") {
+    Airports test_airports;
+    routes test_routes;
+    test_routes.routes_map["A"].first.push_back("B");
+    test_routes.routes_map["A"].second.push_back(1);
+    test_routes.routes_map["A"].first.push_back("C");
+    test_routes.routes_map["A"].second.push_back(3);
+    test_routes.routes_map["B"].first.push_back("C");
+    test_routes.routes_map["B"].second.push_back(5);
+    test_routes.routes_map["B"].first.push_back("D");
+    test_routes.routes_map["B"].second.push_back(4);
+    test_routes.routes_map["D"].first.push_back("C");
+    test_routes.routes_map["D"].second.push_back(4);
+    REQUIRE(dijkstra("A", "C", test_routes.routes_map).first == 3.0);
+    REQUIRE(dijkstra("B", "C", test_routes.routes_map).first == 4.0);
+}
 
 TEST_CASE("Test Read Airport") {
     Airports test_airports;
